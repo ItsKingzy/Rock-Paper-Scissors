ac6a2c78b446a39659e568b1cef19996f9be4bc7
Cameron L. <imcelarson@gmail.com>
2026-03-04 22:21:43 -0700

    Created human choice function'

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 // console.log("Hello world");
 
+// Set array of Rock Paper Scissors
+const rps = ["Rock", "Paper", "Scissors"]
+
+// Computer choice
 function getComputerChoice() {
     let choice = Math.round(Math.random() * 3);
-    if (choice === 1) {
-        console.log("rock");
-    } else if (choice === 2) {
-        console.log("paper");
-    } else if (choice === 3) {
-        console.log("scissors");
-    }
+//     if (choice === 1) {
+//         console.log("rock");
+//     } else if (choice === 2) {
+//         console.log("paper");
+//     } else if (choice === 3) {
+//         console.log("scissors");
+//     }
+
+    return choice;
 }
 
-getComputerChoice();
+// Human choice
+function getHumanChoice() {
+    // Fetch user input (Either Rock, Paper, or Scissors)
+    let response = prompt("Enter either 'Rock', 'Paper', or 'Scissors'");
+    response = response.toLowerCase().trim();
+
+    return response;
+}
+
